287fa4d3baa01d98a31b737debbbfdd479f39bf2
geraldjän <solidcaius@gmail.com>
2020-06-04 17:36:40 +0200

    setup for store and noramlizing

---
+++ src/store/appReducer.js
@@ -0,0 +1,29 @@
+import products from "../data/products";
+
+const normalizedProducts = products.reduce((acc,productsEntry) => {
+    
+    acc[productsEntry.id] = productsEntry;  
+    return acc;
+        
+},{});
+
+const cart = {
+    "products": [],
+    "sum": 0,
+}
+
+const initialState= {
+    normalizedProducts,
+    cart
+}
+
+function appReducer(state = initialState, action){
+    switch (action) {
+        case "ADD_TOCART":
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default appReducer;
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import appReducer from "./appReducer";
+const { createStore, compose, applyMiddleware} = require('redux');
+
+// // Be sure to ONLY add this middleware in development!
+const middleware = process.env.NODE_ENV !== 'production' ?
+  [
+      require('redux-immutable-state-invariant').default(),
+  ]:
+  [];
+ 
+// Note passing middleware as the last argument to createStore requires redux@>=3.1.0
+
+const store = createStore(
+    appReducer,
+    // compose(applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+
+export default store;
